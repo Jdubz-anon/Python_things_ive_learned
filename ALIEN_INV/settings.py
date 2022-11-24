402895fc2a0fcf686e62f700bae7a2dc5528fc69
judzanon <jdubz_programming@mailfence.com>
2022-11-24 09:26:59 -0500

    commit all files and folders; first commit

---
+++ ALIEN_INV/settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    '''creating a settings module to store all game settings'''
+    def __init__(self):
+        '''initializing game settings'''
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        self.ship_speed = 1.5
+
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
