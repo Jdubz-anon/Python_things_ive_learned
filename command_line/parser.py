019d8331f64db13846f410e9a4857888d1c69600
judzanon <jdubz_programming@mailfence.com>
2023-01-20 00:11:12 -0500

    created a new parsinanother file

--- command_line/parser.py
+++ command_line/parser.py
@@ -1,81 +1,40 @@
-#graph line total-crime los-angeles&new-york 2000-2010
-
+import csv
 
 class Parser:
     def __init__(self, input_):
-        self.input = input_.split()
-        self.func_list = []
-        self.graph_type = ['line','bar','pie']
-        self.arg_list = []
-        self.graph = None
-        self.function_dict = {
-                            'graph':"graph",
-                            'showme':"showme",
-                            'list': 'create_list',
-                            'peek':"peek"
-                                            }
-
-
-
-        self.func_arg_dict = {
-                        "graph": None,
-                        "showme":None,
-                        "list": None,
-                        "peek":None,
-
+        self.split_input = input_.split()
+        self.arg_dict = {
+            'location': None,
+            'category': None,
+            'dates': None
             }
+        self.func_dict = {
+            'list': self.create_list
+        }
+        self.func_list = list(filter(lambda func : func in self.func_dict,
+                                     self.split_input))
+    def _arg_organizer(self):
+        self.arg_dict['dates'] = self.split_input[-1].split('-')
+        self.arg_dict['location'] = self.split_input[-2]
+        self.arg_dict['category'] = self.split_input[-3]
 
-    def ampsand_remover(self, *args):
-        new_input = []
-        for item in args:
-            if "&" in item:
-                new_input.append(item.replace('&', ' '))
-            else:
-                new_input.append(item)
-        return new_input
-
-    def function_worker(self, input_):
-        #func_list = []
-        filt_func_list = list(filter(lambda item : item in self.function_dict, self.input))
-        self.func_list.clear()
-        self.func_arg_dict.clear()
-        for item in filt_func_list:
-            item_location = self.input.index(item)
-            self.func_arg_dict[item] = self.ampsand_remover(self.input[item_location +1])
-            self.func_list.append(item)
-
-
-
-    # def dash_remover(self, *args):
-    #     #remove_ampsands = self.ampsand_remover(args)
-    #     func_arg_list = []
-    #     for item in remove_ampsands:
-    #         if "-" in item and not item.isdigit():
-    #             func_arg_list.append(item.replace("-", " "))
-    #         else:
-    #             func_arg_list.append(item)
-    #     return func_arg_list
-
-    
-# testing_input = input('say something: ')
-#
-# par = Parser(testing_input)
-# func = par.function_worker(testing_input)
-# # print(par.ampsand_remover(testing_input))
-# print(*par.func_arg_dict['graph'])
-# print(*par.func_arg_dict['showme'])
-# print(*par.func_arg_dict['list'])
-# print(*par.func_arg_dict['peek'])
+    def create_list(self):
+        self._arg_organizer()
+        file = '/media/jdubzanon/SmallStorage/csv_files/state_crime.csv'
+        with open(file) as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            line = 0
+            for row in csv_reader:
+                for i in range(int(self.arg_dict['dates'][0]), int(self.arg_dict['dates'][1]) + 1):
+                    if self.arg_dict['location'].title() in row.values() and str(i) in row['Year']:
+                        print(row[self.arg_dict['category']] + " " + str(i))
 
 
-    
-    
-    
-    #def word_splitter(self):
-        #for word in self.input:
-            #if word not in self.graph_type and self.function_dict.keys():
-                #self.word_split_dict[word] = word.replace('-', '')
 
 
-    #def location_splitter(self, input):
-        #pass
+
+
+input_ = input("> ")
+par = Parser(input_)
+for items in par.func_list:
+    par.func_dict[items]()
